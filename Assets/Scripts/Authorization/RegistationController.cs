c69b1a8b1ad6ed3bbab1fe620bcc3eb6c93d703c
196PetrovaSasha <ampetrova_7@edu.hse.ru>
2025-04-27 11:17:52 +0300

    фикс регистрации

--- Assets/Scripts/Authorization/RegistationController.cs
+++ Assets/Scripts/Authorization/RegistationController.cs
@@ -1,3 +1,53 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:db9be3c710a2cde35df1de457b853dab96874ee713e01eedd630437c9af89006
-size 1174
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.Events;
+using System.Collections.Generic;
+
+namespace Novel {
+    public class RegistationController : MonoBehaviour
+    {
+        public static RegistationController instance;
+        public TMP_InputField email;
+        public TMP_InputField password;
+
+        public TMP_InputField name;
+
+        public TMP_Text text_error;
+        public UnityAction<string> error;
+
+        void OnEnable() {
+            this.error += HandleError;
+        }
+
+        void OnDisable() {
+            this.error += HandleError;
+        }
+
+        void Awake() {
+            instance = this;
+        }
+
+        void Start()
+        {
+            instance = this;
+        }
+
+
+        public void Authorize() {
+                HttpClient.instance.Request<SignResponse>(HttpRequestType.Post,
+                HttpClient.signup_url, 
+                new Dictionary<string, string>(){{"email", email.text}, {"password", password.text}, {"name", name.text}}, 
+                UserController.instance.authorized, 
+                error
+            );
+            
+        }
+
+        public void HandleError(string text) {
+            text_error.text = text;
+        }
+
+        
+    }
+}
